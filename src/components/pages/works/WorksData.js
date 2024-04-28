debddacd24a039dd9c2af0cd1c29989c9e0f252f
yoriss67 <109190795+yoriss67@users.noreply.github.com>
2024-04-28 09:58:11 +0900

    Add Knowledge Work

--- src/components/pages/works/WorksData.js
+++ src/components/pages/works/WorksData.js
@@ -1,46 +1,71 @@
+/** @format */
+
 const WorksData = [
   {
     id: 1,
-    date: 'Nov 2023',
-    toolLogo: 'Figma logo.svg',
-    periodJP: '1.5ヶ月',
-    periodEN: '1.5 month',
-    semiTitleJP: 'AIとのチャットで最適な自己分析と企業とのマッチングを実現するアプリ',
-    semiTitleEN: 'An app that realizes the best self-analysis and matching with companies through chat with A',
-    titleJP: 'Path Finder',
-    titleEN: 'Path Finder',
-    image1: 'works__images/Path Finder1.png',
-    image2: 'works__images/Path Finder2.png',
-    alt: 'AI job hunt app',
-    descJP: '知り合いの紹介でこの提案をしてくださった方は企業にビジネスアイデアを提出しようとしており、「AIとのチャットx就活サービス」というアイデアからこのサービスを考えついたそうです。そのアイデアをデザイン化し、企業の担当者にアピールしたいとのことでご依頼をいただきました。',
-    descEN: 'The person who proposed this idea was trying to submit a business idea to a company, and came up with this service from the idea of "AI chat x job hunting service" that was introduced by an acquaintance. I was asked to design the idea and appeal to the company.',
-    className: 'path-finder',
-    url: 'https://student.redesigner.jp/portfolios/PFd88efd577cf64d17b8dcb17fe9062918',
+    date: "Feb 2024",
+    toolLogo: "Figma logo.svg",
+    periodJP: "2日",
+    periodEN: "2 days",
+    semiTitleJP: "スマホで簡単日程調整を実現する営業支援アプリ",
+    semiTitleEN:
+      "An app that realizes easy schedule adjustment on a smartphone",
+    titleJP: "2DAY Internship",
+    titleEN: "2DAY Internship at Knowledge Work Inc.",
+    image1: "kw__images/Knowledge Work1.png",
+    image2: "kw__images/Knowledge Work2.png",
+    alt: "Knowledge Work Inc. 2DAY Internship",
+    descJP:
+      "ユーザーは予定を組むため日程の候補を絞り込み、同伴者へのリクエスト送信までを完結できます。たとえ対面の予定を組む場合でも、移動時間を自動的に計算し最適な候補日程を提案します。",
+    descEN:
+      "User can narrow down the criteria, select candidate dates and complete the process of sending a request to the accompanying person. Even if you are planning a face-to-face meeting, the system automatically calculates travel time and suggests the most suitable dates.",
+    className: "wk-internship",
+    url: "https://student.redesigner.jp/portfolios/PF9d7dfa53d14acd838e704c93852e9adc",
   },
 
   {
     id: 2,
-    date: 'Nov 2023',
-    toolLogo: 'Figma logo.svg',
-    periodJP: '1週間',
-    periodEN: '1 week',
-    semiTitleJP: 'インターンシップ選考課題 家計簿アプリ ',
-    semiTitleEN: 'Internship selection task Household account book app',
-    titleJP: 'Asset Forest',
-    titleEN: 'Asset Forest',
-    image1: 'works__images/Money Forest1.png',
-    image2: 'works__images/Money Forest2.png',
-    alt: 'household account book app',
-    descJP: 'インターンシップ選考課題として、家計簿アプリを作成しました。初めて１週間という短い期間でデザインを考えました。現行のアプリの調査やユーザーに求められる機能の選出など、反省点や改善点は多くあるものの自分なりのアウトプットに落とし込むことができました。',
-    descEN: 'I created a household account book app as an internship selection task. I designed it in a short period of one week for the first time. Although there are many points of reflection and improvement, such as investigating the current app and selecting the functions required by the user, I was able to incorporate it into my own output.',
-    className: 'money-forest',
-    url: 'https://student.redesigner.jp/portfolios/PFff143303e9869f7d5a42e0f57c95dc21',
+    date: "Nov 2023",
+    toolLogo: "Figma logo.svg",
+    periodJP: "1.5ヶ月",
+    periodEN: "1.5 month",
+    semiTitleJP:
+      "AIとのチャットで最適な自己分析と企業とのマッチングを実現するアプリ",
+    semiTitleEN:
+      "An app that realizes the best self-analysis and matching with companies through chat with A",
+    titleJP: "Path Finder",
+    titleEN: "Path Finder",
+    image1: "works__images/Path Finder1.png",
+    image2: "works__images/Path Finder2.png",
+    alt: "AI job hunt app",
+    descJP:
+      "知り合いの紹介でこの提案をしてくださった方は企業にビジネスアイデアを提出しようとしており、「AIとのチャットx就活サービス」というアイデアからこのサービスを考えついたそうです。そのアイデアをデザイン化し、企業の担当者にアピールしたいとのことでご依頼をいただきました。",
+    descEN:
+      'The person who proposed this idea was trying to submit a business idea to a company, and came up with this service from the idea of "AI chat x job hunting service" that was introduced by an acquaintance. I was asked to design the idea and appeal to the company.',
+    className: "path-finder",
+    url: "https://student.redesigner.jp/portfolios/PFd88efd577cf64d17b8dcb17fe9062918",
   },
 
-
-
+  {
+    id: 3,
+    date: "Nov 2023",
+    toolLogo: "Figma logo.svg",
+    periodJP: "1週間",
+    periodEN: "1 week",
+    semiTitleJP: "インターンシップ選考課題 家計簿アプリ ",
+    semiTitleEN: "Internship selection task Household account book app",
+    titleJP: "Asset Forest",
+    titleEN: "Asset Forest",
+    image1: "works__images/Money Forest1.png",
+    image2: "works__images/Money Forest2.png",
+    alt: "household account book app",
+    descJP:
+      "インターンシップ選考課題として、家計簿アプリを作成しました。初めて１週間という短い期間でデザインを考えました。現行のアプリの調査やユーザーに求められる機能の選出など、反省点や改善点は多くあるものの自分なりのアウトプットに落とし込むことができました。",
+    descEN:
+      "I created a household account book app as an internship selection task. I designed it in a short period of one week for the first time. Although there are many points of reflection and improvement, such as investigating the current app and selecting the functions required by the user, I was able to incorporate it into my own output.",
+    className: "money-forest",
+    url: "https://student.redesigner.jp/portfolios/PFff143303e9869f7d5a42e0f57c95dc21",
+  },
 ];
 
 export default WorksData;
-
-
